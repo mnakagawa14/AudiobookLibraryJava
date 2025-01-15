803b8b56112f5cb85b80aff4efa0edde40e2aa15
mnakagawa14 <134327026+mnakagawa14@users.noreply.github.com>
2025-01-14 18:39:31 -1000

    Initial commit

---
+++ src/main/java/org/github/mnakagawa14/audiobooklibraryjava/AudiobookLibrary.java
@@ -0,0 +1,8 @@
+package org.github.mnakagawa14.audiobooklibraryjava;
+
+public class AudiobookLibrary {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
